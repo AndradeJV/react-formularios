e190dd660d034d9b058512753b7d56f3d2001cac
Joao Andrade <joaovandrade30@gmail.com>
2021-10-14 13:23:04 -0300

    Configs realizadas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import AdicionaCliente from './components/AdicionaCliente';
+import AdicionaCliente from './components/AdicionaCliente/AdicionarCliente';
 
 function App() {
   return (
